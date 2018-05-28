1e9fccb888d13dbd9a48f6e6a60f162a0d34cc66
Paulo Fernandes <9150293@esmad.ipp.pt>
2018-05-28 14:47:20 +0100

    update teste

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -17,7 +17,7 @@ router.get('/cars', function (req, res) {
 });
 
 router.get('/help', function (req, res) {
-    dados = Dados.get(req, res)
+    //dados = Dados.get(req, res)
 
     res.status(200).send({'doc':"https://repositorio.ipcb.pt/bitstream/10400.11/408/1/Tese%20Mestrado%20Ant%C3%B3nio%20Gil.pdf"})  
 });
