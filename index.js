a53c188f50500962dc61139b11e064c65d988da3
Paulo Fernandes <9150293@esmad.ipp.pt>
2018-06-04 15:31:49 +0100

    update

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var http = require('http');
 
 
 var app = express();
-app.use(cors({
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-}));
+// app.use(cors({
+//     'allowedHeaders': ['sessionId', 'Content-Type'],
+//     'exposedHeaders': ['sessionId'],
+//     'origin': '*',
+//     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     'preflightContinue': false
+// }));
 
 app.use(function (req, res, next) {
 
