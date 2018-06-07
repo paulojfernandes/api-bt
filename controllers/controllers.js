e6499e75ccd3480231823529438876a3f9bad476
Paulo Fernandes <9150293@esmad.ipp.pt>
2018-06-07 14:09:56 +0100

    update

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -10,8 +10,6 @@ router.use(bodyParser.urlencoded({
 router.use(bodyParser.json())
 
 
-
-
 /*router.get('/cars', function (req, res) {
     dados = Dados.get(req, res)
 });
@@ -25,58 +23,60 @@ router.get('/stock', function (req, res) {
     dados = Dados.getCarsStock(req, res)
 });
 
-
 router.post('/stock', function (req, res) {
     dados = Dados.postSpecificCarStock(req, res)
 });
 
-
-
-router.post('/salesDay', function(req, res){
+router.post('/salesDay', function (req, res) {
     dados = Dados.postTotalSalesDay(req, res)
 })
 
-router.get('/sales', function(req, res){
+router.get('/sales', function (req, res) {
     dados = Dados.getCarSales(req, res)
 })
 
-router.post('/sales', function(req, res){
+router.post('/sales', function (req, res) {
     dados = Dados.postSales(req, res)
 })
 
-router.get('/clients', function(req, res){
+router.get('/clients', function (req, res) {
     dados = Dados.getClients(req, res)
 })
 
 
-router.post('/clients', function(req, res){
+router.post('/clients', function (req, res) {
     dados = Dados.postSpecificClient(req, res)
 })
 
-router.get('/brands', function(req, res){
+router.get('/brands', function (req, res) {
     dados = Dados.getBrands(req, res)
 })
 
 
-router.get('/categories', function(req, res){
+router.get('/categories', function (req, res) {
     dados = Dados.getCategories(req, res)
 })
 
 
-router.post('/addCar', function(req, res){
+router.post('/addCar', function (req, res) {
     dados = Dados.postAddCar(req, res)
 })
 
-router.post('/addClient', function(req, res){
+router.post('/addClient', function (req, res) {
     dados = Dados.addClient(req, res)
 })
 
+router.post('sellCar', function (req, res) {
+    dados = Dados.sellCar(req, res);
+});
 
 
 router.get('/help', function (req, res) {
     //dados = Dados.get(req, res)
 
-    res.status(200).send({'doc':"https://repositorio.ipcb.pt/bitstream/10400.11/408/1/Tese%20Mestrado%20Ant%C3%B3nio%20Gil.pdf"})  
+    res.status(200).send({
+        'doc': "https://repositorio.ipcb.pt/bitstream/10400.11/408/1/Tese%20Mestrado%20Ant%C3%B3nio%20Gil.pdf"
+    })
 });
 
 
--- models/models.js
+++ models/models.js
@@ -17,13 +17,13 @@ var config = {
 
 console.log("entrei models");
 
-exports.getCarsStock = function(req, res) {
-    var query = 'SELECT marca, modelo, categoria,ano, extras,img,venda.vendido,preço,cilindrada, matricula, km,cor FROM venda, marca, modelo, extras, categoria WHERE venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras AND venda.vendido = 0;';
+exports.getCarsStock = function (req, res) {
+    var query = 'SELECT  id_venda,marca, modelo, categoria,ano, extras,img,venda.vendido,preço,cilindrada, matricula, km,cor FROM venda, marca, modelo, extras, categoria WHERE venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras AND venda.vendido = 0;';
     queryStandard(query, req, res);
 };
 
 // Obter carros especificos ( Vendidos Incluidos )
-exports.postSpecificCar = function(req, res) {
+exports.postSpecificCar = function (req, res) {
     var param = {};
     var whereQuery = " ";
 
@@ -35,20 +35,19 @@ exports.postSpecificCar = function(req, res) {
     for (var key in param) {
         if (key == "matricula") {
             whereQuery += "venda.matricula='" + param[key] + "' AND ";
-        }
-        else {
+        } else {
             whereQuery += key + '.' + key + "='" + param[key] + "' AND ";
         }
     }
 
-    var query = 'SELECT marca, modelo, categoria,ano, extras,img,venda.vendido,preço,cilindrada, matricula, km,cor FROM   venda, vendidos, marca, modelo, extras,categoria WHERE' + whereQuery + ' venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras; ';
+    var query = 'SELECT id_venda,marca, modelo, categoria,ano, extras,img,venda.vendido,preço,cilindrada, matricula, km,cor FROM   venda, vendidos, marca, modelo, extras,categoria WHERE' + whereQuery + ' venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras; ';
     queryStandard(query, req, res);
 };
 
 
 
 // Obter carros especifico em stock 
-exports.postSpecificCarStock = function(req, res) {
+exports.postSpecificCarStock = function (req, res) {
 
     var param = {};
     var whereQuery = " ";
@@ -58,33 +57,32 @@ exports.postSpecificCarStock = function(req, res) {
     for (var key in param) {
         if (key == "matricula") {
             whereQuery += "venda.matricula='" + param[key] + "' AND ";
-        }
-        else {
+        } else {
             whereQuery += key + '.' + key + "='" + param[key] + "' AND ";
         }
     }
-    var query = 'SELECT marca, modelo, categoria,ano, extras,img,venda.vendido,preço,cilindrada, matricula, km,cor FROM venda, marca, modelo, extras, categoria WHERE' + whereQuery + ' venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras  AND venda.vendido = 0; ';
+    var query = 'SELECT id_venda,marca, modelo, categoria,ano, extras,img,venda.vendido,preço,cilindrada, matricula, km,cor FROM venda, marca, modelo, extras, categoria WHERE' + whereQuery + ' venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras  AND venda.vendido = 0; ';
 
     queryStandard(query, req, res);
 
 };
 
-exports.getCarSales = function(req, res) {
+exports.getCarSales = function (req, res) {
 
 
-    var query = 'SELECT marca, modelo, categoria,img, venda.vendido, cilindrada, matricula, cor, data_venda, nif FROM venda, marca, modelo, extras, categoria, vendidos, cliente WHERE venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras AND venda.vendido = 1 and vendidos.id_cliente= cliente.id_cliente;';
+    var query = 'SELECT id_venda, marca, modelo, categoria,img, venda.vendido, cilindrada, matricula, cor, data_venda, nif FROM venda, marca, modelo, extras, categoria, vendidos, cliente WHERE venda.id_categoria = categoria.id_categoria AND venda.id_marca = marca.id_marca AND venda.id_modelo = modelo.id_modelo AND venda.id_extras = extras.id_extras AND venda.vendido = 1 and vendidos.id_cliente= cliente.id_cliente;';
     queryStandard(query, req, res);
 
 };
 
-exports.postTotalSalesDay = function(req, res) {
+exports.postTotalSalesDay = function (req, res) {
 
     var data = req.body.data;
     var query = "SELECT COUNT(id_vendido) as total FROM vendidos WHERE DATE_FORMAT(vendidos.data_venda, '%d/%m/%Y') = '" + data + "';";
     queryStandard(query, req, res);
 };
 
-exports.postSales = function(req, res) {
+exports.postSales = function (req, res) {
 
     var param = {};
 
@@ -97,8 +95,7 @@ exports.postSales = function(req, res) {
     for (var key in param) {
         if (key == "matricula") {
             whereQuery += "venda.matricula='" + param[key] + "' AND ";
-        }
-        else {
+        } else {
             fromQuery += "," + key + " ";
             //whereQuery += key + '.' + key + "='" + param[key] + "' AND "
             whereQuery += key + '.' + key + "='" + param[key] + "' AND  venda.id_" + key + '=' + key + '.id_' + key + ' AND';
@@ -110,12 +107,12 @@ exports.postSales = function(req, res) {
 
 
 
-exports.getClients = function(req, res) {
+exports.getClients = function (req, res) {
     var query = 'select * from cliente, morada where cliente.id_morada= morada.id_morada; ';
     queryStandard(query, req, res);
 };
 
-exports.postSpecificClient = function(req, res) {
+exports.postSpecificClient = function (req, res) {
 
     var param = {};
     var whereQuery = " ";
@@ -132,14 +129,14 @@ exports.postSpecificClient = function(req, res) {
 };
 
 
-exports.getBrands = function(req, res) {
+exports.getBrands = function (req, res) {
     var query = 'SELECT * FROM marca ';
 
     queryStandard(query, req, res);
 
 };
 
-exports.getCategories = function(req, res) {
+exports.getCategories = function (req, res) {
     var query = 'SELECT * FROM categoria ';
 
     queryStandard(query, req, res);
@@ -147,7 +144,7 @@ exports.getCategories = function(req, res) {
 };
 
 
-exports.postAddCar = function(req, res) {
+exports.postAddCar = function (req, res) {
     console.log(req.body);
 
     var id_marca = req.body.id_marca;
@@ -166,48 +163,44 @@ exports.postAddCar = function(req, res) {
 
     var connection = new mysql.createConnection(config);
     connection.connect(
-        function(err) {
+        function (err) {
             if (err) {
                 console.log("!!! Cannot connect !!! Error:");
                 throw err;
-            }
-            else {
+            } else {
                 console.log("Connection established.");
 
             }
         });
 
 
-    connection.query('INSERT INTO modelo(modelo) values("' + modelo + '");', function(err, rows, fields) {
+    connection.query('INSERT INTO modelo(modelo) values("' + modelo + '");', function (err, rows, fields) {
         if (!err) {
             //res.status(200).send(rows)
             console.log("Modelo Inserido");
-        }
-        else {
+        } else {
             res.status(500).send("Erro  Modelo");
             console.log('Error while performing Query.', err);
         }
 
     });
 
-    connection.query('INSERT INTO extras(extras) values("' + extras + '");', function(err, rows, fields) {
+    connection.query('INSERT INTO extras(extras) values("' + extras + '");', function (err, rows, fields) {
         if (!err) {
             //res.status(200).send(rows)
             console.log("Extras Inserido");
-        }
-        else {
+        } else {
             res.status(500).send("Erro BD Extras");
             console.log('Error while performing Query.', err);
         }
 
     });
 
-    connection.query('INSERT INTO venda(id_marca, id_modelo,id_categoria,id_extras,preço, km, ano, img, cilindrada, matricula, cor, vendido) values(' + id_marca + ',(select max(id_modelo) from modelo),' + id_categoria + ',(select max(id_extras) from extras),' + preço + ',' + km + ',' + ano + ',"' + img + '", "' + cilindrada + '","' + matricula + '", "' + cor + '", 0);', function(err, rows, fields) {
+    connection.query('INSERT INTO venda(id_marca, id_modelo,id_categoria,id_extras,preço, km, ano, img, cilindrada, matricula, cor, vendido) values(' + id_marca + ',(select max(id_modelo) from modelo),' + id_categoria + ',(select max(id_extras) from extras),' + preço + ',' + km + ',' + ano + ',"' + img + '", "' + cilindrada + '","' + matricula + '", "' + cor + '", 0);', function (err, rows, fields) {
         if (!err) {
             res.status(200).send(rows);
             console.log("Carro Inserido");
-        }
-        else {
+        } else {
             res.status(500).send("Erro BD");
             console.log('Error while performing Query.', err);
         }
@@ -216,7 +209,75 @@ exports.postAddCar = function(req, res) {
 
 };
 
-exports.addClient = function(req, res) {
+exports.sellCar = function (req, res) {
+
+
+    // var connection = new mysql.createConnection(config);
+    // connection.connect(
+    //     function (err) {
+    //         if (err) {
+    //             console.log("!!! Cannot connect !!! Error:");
+    //             throw err;
+    //         } else {
+    //             console.log("Connection established.");
+
+    //         }
+    //     });
+
+    /* se o cliente já existe
+     ** update aos dados do cliente
+     ** id do cliente 
+     ** id da venda(CARRO)-- ADICIONAR AO PEDIDO 
+     ** preço final 
+     */
+
+
+
+
+
+
+    /* se o cliente não existe
+    ** inserir morada
+    ** inserir cliente
+    ** inserir nos vendidos          
+     
+    */
+
+
+
+    // connection.query('INSERT INTO morada(morada, cidade,distrito, país) values("' + morada + '", "' + cidade + '", "' + distrito + '", "Portugal");', function (err, rows, fields) {
+    //     if (!err) {
+    //         //res.status(200).send(rows)
+    //         console.log("Morada Inserida");
+    //     } else {
+    //         res.status(500).send("Erro  Modelo");
+    //         console.log('Error while performing Query.', err);
+    //     }
+
+    // });
+
+    // connection.query('INSERT INTO cliente(id_morada, nif, telemovel, nome) values((select max(id_morada) from morada),"' + nif + '","' + telemovel + '","' + nome + '");', function (err, rows, fields) {
+    //     if (!err) {
+    //         res.status(200).send("Cliente Inserido")
+    //         console.log("Cliente Inserido");
+    //     } else {
+    //         res.status(500).send("Erro BD Extras");
+    //         console.log('Error while performing Query.', err);
+    //     }
+    //     connection.end();
+    // });
+
+
+
+
+
+
+}
+
+
+
+
+exports.addClient = function (req, res) {
 
     console.log(req.body);
 
@@ -231,36 +292,33 @@ exports.addClient = function(req, res) {
 
     var connection = new mysql.createConnection(config);
     connection.connect(
-        function(err) {
+        function (err) {
             if (err) {
                 console.log("!!! Cannot connect !!! Error:");
                 throw err;
-            }
-            else {
+            } else {
                 console.log("Connection established.");
 
             }
         });
 
 
-    connection.query('INSERT INTO morada(morada, cidade,distrito, país) values("'+morada+'", "'+cidade+'", "'+distrito+'", "Portugal");', function(err, rows, fields) {
+    connection.query('INSERT INTO morada(morada, cidade,distrito, país) values("' + morada + '", "' + cidade + '", "' + distrito + '", "Portugal");', function (err, rows, fields) {
         if (!err) {
             //res.status(200).send(rows)
             console.log("Morada Inserida");
-        }
-        else {
+        } else {
             res.status(500).send("Erro  Modelo");
             console.log('Error while performing Query.', err);
         }
 
     });
 
-    connection.query('INSERT INTO cliente(id_morada, nif, telemovel, nome) values((select max(id_morada) from morada),"'+nif+'","'+telemovel+'","'+nome+'");', function(err, rows, fields) {
+    connection.query('INSERT INTO cliente(id_morada, nif, telemovel, nome) values((select max(id_morada) from morada),"' + nif + '","' + telemovel + '","' + nome + '");', function (err, rows, fields) {
         if (!err) {
             res.status(200).send("Cliente Inserido")
             console.log("Cliente Inserido");
-        }
-        else {
+        } else {
             res.status(500).send("Erro BD Extras");
             console.log('Error while performing Query.', err);
         }
@@ -277,22 +335,20 @@ exports.addClient = function(req, res) {
 function queryStandard(query, req, res) {
     var connection = new mysql.createConnection(config);
     connection.connect(
-        function(err) {
+        function (err) {
             if (err) {
                 console.log("!!! Cannot connect !!! Error:");
                 throw err;
-            }
-            else {
+            } else {
                 console.log("Connection established.");
 
             }
         });
-    connection.query(query, function(err, rows, fields) {
+    connection.query(query, function (err, rows, fields) {
         if (!err) {
             res.status(200).send(rows);
             console.log("entrei");
-        }
-        else {
+        } else {
             res.status(500).send("Erro BD");
             console.log('Error while performing Query.', err);
         }
